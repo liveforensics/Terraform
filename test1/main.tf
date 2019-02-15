5e419d66ef42278ee51d11df41b8d1c87b149a5b
mark <liveforensics@mark-morley.co.uk>
2019-02-15 10:38:18 +0000

    housekeeping

--- test1/main.tf
+++ test1/main.tf
@@ -1,9 +1,4 @@
-provider "azurerm" {
-  subscription_id = "4dcbed93-ddd0-42a0-aa79-44208d362580"
-  client_id       = "537ea2b6-90d7-47ae-ba44-fe9ab5887b91"
-  client_secret   = "3388191b-9694-44ad-a90d-e791eb04a90c"
-  tenant_id       = "4e73027a-db9b-4958-9a63-06330ea6e88f"
-}
+
 
 # locals {
 #   virtual_machine_name = "${var.prefix}vm"
