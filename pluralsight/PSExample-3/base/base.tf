57e8a474bcbd2a2da627356123ba90e282de6bd6
mark <liveforensics@mark-morley.co.uk>
2019-04-17 10:53:13 +0100

    vd

--- pluralsight/PSExample-3/base/base.tf
+++ pluralsight/PSExample-3/base/base.tf
@@ -1,1 +1,8 @@
 # simple vsphere example
+
+provider "vSphere" {
+    user = "pluralsight@vsphere.local"
+    password = "P@ssw0rd"
+    vsphere_server = "192.168.0.30"
+    allow_unverified_ssl = true
+}
