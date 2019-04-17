4e79a4ce43330a354afe0b777562a8a633d7ff03
mark <liveforensics@mark-morley.co.uk>
2019-04-17 11:02:22 +0100

    vdxc

--- pluralsight/PSExample-3/base/base.tf
+++ pluralsight/PSExample-3/base/base.tf
@@ -1,8 +1,14 @@
 # simple vsphere example
 
-provider "vSphere" {
+provider "vsphere" {
     user = "pluralsight@vsphere.local"
     password = "P@ssw0rd"
     vsphere_server = "192.168.0.30"
     allow_unverified_ssl = true
+}
+
+# create a folder
+resource "vsphere_folder" "TerraformFrontEnd" {
+  datacenter = "Datacenter"
+  path = "TerraformFrontEnd"
 }
