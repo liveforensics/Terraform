c1953ccead0100b2f00a919a2c6bedfe53bd255d
mark <liveforensics@mark-morley.co.uk>
2019-11-10 18:01:04 +0000

    ccz\xc

--- Azure/liveforensics/modules/windows-client/files/winrm.ps1
+++ Azure/liveforensics/modules/windows-client/files/winrm.ps1
@@ -16,10 +16,12 @@
 
 # Write-Host "Open Firewall Port"
 # netsh advfirewall firewall add rule name="Windows Remote Management (HTTPS-In)" dir=in action=allow protocol=TCP localport=5985
-Write-Host "Enabling WinRM - I hope **************************************************************************************************************"
+Write-Host "Enabling WinRM - I hope **************************************************************************************************************" | Out-File c:\terraform\log.txt
 Enable-PSRemoting -Force
 
 Write-Host "Open Firewall Port"
 netsh advfirewall firewall add rule name="Windows Remote Management (HTTPS-In)" dir=in action=allow protocol=TCP localport=5985
 Start-Service "WinRM"
-Set-NetFirewallRule -DisplayGroup "Windows Remote Management" -Enabled True -Profile Public
+# Set-NetFirewallRule -DisplayGroup "Terraform" -Enabled True -Profile Public
+New-NetFirewallRule -DisplayName "Allow Terraforming" -Direction Inbound -Action Allow -EdgeTraversalPolicy Allow -Protocol TCP -LocalPort 5985
+# Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
--- clean.ps1
+++ clean.ps1
@@ -1,1 +1,4 @@
 terraform destroy --var-file="..\Common\terraform.tfvars" -auto-approve
+Remove-Item -Force terraform.tfplan
+Remove-Item -Force terraform.tfstate
+Remove-Item -Force terraform.tfstate.backup
