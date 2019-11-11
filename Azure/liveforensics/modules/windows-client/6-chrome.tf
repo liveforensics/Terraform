878b2e49b2f31fb6b92c12b54cb4cd63e5f5e4a2
mark <liveforensics@mark-morley.co.uk>
2019-11-11 22:50:13 +0000

    ft

--- Azure/liveforensics/modules/windows-client/6-chrome.tf
+++ Azure/liveforensics/modules/windows-client/6-chrome.tf
@@ -1,19 +1,19 @@
 resource "null_resource" "file-copy-test" {
-  provisioner "remote-exec" {
-    connection {
-      host     = "loveforensics.ukwest.cloudapp.azure.com"
-      type     = "winrm"
-      user     = "colin"
-      port     = "5985"
-      password = "terraformP@ssw0rd"
-      timeout  = "5m"
-      https    = "false"
-      use_ntlm = "true"
-    }
-    inline = [
-      "cmd /C \"powershell.exe set-executionpolicy unrestricted\"",
-    "cmd /C \"powershell.exe New-Item -Type Directory c:\\deleteme \""]
-  }
+  #   provisioner "remote-exec" {
+  #     connection {
+  #       host     = "loveforensics.ukwest.cloudapp.azure.com"
+  #       type     = "winrm"
+  #       user     = "colin"
+  #       port     = "5985"
+  #       password = "terraformP@ssw0rd"
+  #       timeout  = "5m"
+  #       https    = "false"
+  #       use_ntlm = "true"
+  #     }
+  #     inline = [
+  #       "cmd /C \"powershell.exe set-executionpolicy unrestricted\"",
+  #     "cmd /C \"powershell.exe New-Item -Type Directory c:\\deleteme \""]
+  #   }
 
   provisioner "file" {
     connection {
@@ -26,7 +26,7 @@ resource "null_resource" "file-copy-test" {
       use_ntlm = "true"
       port     = "5985"
     }
-    source      = "/downloads/"
+    source      = "E:\\Code\\github\\Terraform\\Azure\\liveforensics\\modules\\windows-client\\downloads\\"
     destination = "C:\\users\\colin\\downloads\\"
   }
   depends_on = ["null_resource.wait-for-domain-to-provision"]
