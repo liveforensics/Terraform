ff58a60493967f8f3e3a82435be1e2f272515712
mark <liveforensics@mark-morley.co.uk>
2019-11-16 15:29:24 +0000

    fds

--- parse-state.ps1
+++ parse-state.ps1
@@ -6,6 +6,6 @@ foreach($item in $json.resources)
     # Write-Host $item.module
     if($item.module -eq "module.windows-client" -and $item.type -eq "azurerm_public_ip")
     {
-        Write-Host $item.module.azurerm_public_ip.static
+        Write-Host $item.instances[0].attributes.fqdn
     }
 }
